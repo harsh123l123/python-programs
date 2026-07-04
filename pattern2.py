d1275b3f75ea11b6bae82517091e65760a9dac5a
harsh123l123 <harshlakhani2005@gmail.com>
2026-07-04 20:27:19 +0530

    pattern

---
+++ pattern2.py
@@ -0,0 +1,9 @@
+n = int(input("enter a number :"))
+for i in range(n+1):
+  for j in range(i):
+    print("*",end=" ")
+  print(" ")  
+for i in range(n+1):
+  for j in range(i):
+    print(" "*n,end=" ")
+  print("*")  
